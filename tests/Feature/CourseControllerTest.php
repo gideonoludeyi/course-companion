9c28284e5f05effb37f11738cb84aa71f6a17fad
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-09-14 17:48:39 -0400

    test CourseController

---
+++ tests/Feature/CourseControllerTest.php
@@ -0,0 +1,136 @@
+<?php
+
+use App\Models\Course;
+use App\Models\User;
+use function Pest\Laravel\{actingAs};
+
+
+it('displays all courses', function () {
+    $user = User::factory()->create();
+
+    $response = actingAs($user)
+        ->get(route('courses.index'));
+
+    $response->assertStatus(200);
+    $response->assertViewIs('courses.index');
+    $response->assertViewHas('courses', Course::all());
+});
+
+it('displays a course with the given id', function () {
+    $user = User::factory()->create();
+    /** @var Course $course */
+    $course = Course::factory()->create();
+
+    $response = actingAs($user)
+        ->get(route('courses.show', $course->id));
+
+    $response->assertStatus(200);
+    $response->assertViewIs('courses.show');
+    $response->assertViewHas('course', $course);
+});
+
+it('displays a course with the given code', function () {
+    $user = User::factory()->create();
+    /** @var Course $course */
+    $course = Course::factory()->create();
+
+    $response = actingAs($user)
+        ->get(route('courses.findCourse', ['code' => $course->code]));
+
+    $response->assertStatus(200);
+    $response->assertViewIs('courses.show');
+    $response->assertViewHas('course', $course);
+});
+
+it('fails to create course for non-advisor', function () {
+    $user = User::factory()->create([
+        'isAdvisor' => false
+    ]);
+    $data = [
+        'code' => 'COSC 1P02',
+        'name' => 'Introduction to Computer Science',
+        'duration' => 'D2',
+    ];
+
+    $response = actingAs($user)
+        ->post(route('courses.store'), $data);
+
+    $response->assertStatus(403);
+});
+
+it('creates a course for advisor', function () {
+    $user = User::factory()->create([
+        'isAdvisor' => true
+    ]);
+    $data = [
+        'code' => 'COSC 1P02',
+        'name' => 'Introduction to Computer Science',
+        'duration' => 'D2',
+    ];
+
+    $response = actingAs($user)
+        ->post(route('courses.store'), $data);
+
+    $response->assertStatus(302);
+    $this->assertDatabaseCount('courses', 1);
+    $this->assertDatabaseHas('courses', $data);
+});
+
+it('fails to update course created by another user', function () {
+    $user = User::factory()->create(['isAdvisor' => false]);
+    $course = Course::factory()
+        ->for(User::factory()->create(['isAdvisor' => true]))
+        ->create([
+            'code' => 'COSC 1P02',
+            'name' => 'Introduction to Computer Science',
+            'duration' => 'D2',
+        ]);
+
+    $response = actingAs($user)
+        ->patch(route('courses.update', $course), [
+            'name' => 'Intro to CS',
+            'code' => 'COSC 1P02',
+            'duration' => 'D2',
+        ]);
+
+    $response->assertStatus(403);
+});
+
+it('updates course for advisor', function () {
+    /** @var User $user */
+    $user = User::factory()->create([
+        'isAdvisor' => true
+    ]);
+    $course = Course::factory()->for($user)->create([
+        'code' => 'COSC 1P02',
+        'name' => 'Introduction to Computer Science',
+        'duration' => 'D2',
+    ]);
+
+    $response = actingAs($user)
+        ->patch(route('courses.update', $course), [
+            'name' => 'Intro to CS',
+            'code' => 'COSC 1P02',
+            'duration' => 'D2',
+        ]);
+
+    $response->assertStatus(302);
+    $response->assertRedirectToRoute('courses.index');
+});
+
+it('deletes a course with the given id', function () {
+    /** @var User $user */
+    $user = User::factory()->create();
+    $course = Course::factory()->for($user)->create([
+        'code' => 'COSC 1P02',
+        'name' => 'Introduction to Computer Science',
+        'duration' => 'D2',
+    ]);
+
+    $response = actingAs($user)
+        ->delete(route('courses.destroy', $course));
+
+    $response->assertStatus(302);
+    $response->assertRedirectToRoute('courses.index');
+    $this->assertDatabaseCount('courses', 0);
+});
